f8b8e515f686f34ac6c492a608f048f651773105
Hancooler <187006072@student.unsil.ac.id>
2021-01-29 13:27:12 +0700

    fix

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,14 +2,14 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = '';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
         <h1>Selamat Datang Di Gundam Center</h1>
 
-        <p class="lead">Silahkan kelola News</p>
+        <p class="lead">Halaman kelola News</p>
 
         <p><a class="btn btn-lg btn-success" href="index.php?r=news">Create</a></p>
     </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -13,7 +13,7 @@ $this->title ='🤖GundamCenter';
             <div class="row">
                 <div class="col-lg-12">
                     <div class="intro-message">
-                        <h1>Selamat Datang </h1>
+                        <h1> Afiliasi </h1>
                         <h3>Gundam Center</h3>
                         
                     
@@ -32,19 +32,21 @@ $this->title ='🤖GundamCenter';
     <!-- Page Content -->
 
 	
-    <div class="content-section-a"style="background-color:white;"  >
+    <!-- /.content-section-a -->
+
+    <div class="content-section-b"style="background-color:white;" >
 
         <div class="container">
+
             <div class="row">
-                <div class="col-lg-5 col-sm-6">
+                <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6" >
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2>Catalog For you</h2>
-                    <p class="lead">Coba deh lihat dulu <a target="_blank" href=""></a> Siapa tahu anda mobile suit kesukaan kamu di <a target="_blank" href="https://www.instagram.com/unicorn_toys/?hl=id">link</a> ini.</p></p> 
+                    <h2>Afiliasi</h2>
+                    <p class="lead">anda akan terhubung ke media sosial disana anda bisa melihat tipe-tipe mobile suit gundam <a target="_blank" href="https://www.instagram.com/unicorn_toys/">link</a>. </p>
                 </div>
-                
-                <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                    <img class="img-responsive" src="https://image.freepik.com/free-vector/blogging-flat-design-concept_257312-766.jpg" alt="" style="width:442px;height:440px;">
+                <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                    <img class="img-responsive" src="https://image.freepik.com/free-vector/attention-attraction-important-announcement-warning-information-sharing-latest-news-loudspeaker-megaphone-bullhorn-with-exclamation-mark-vector-isolated-concept-metaphor-illustration_335657-2809.jpg" alt="" style="width:442px;height:440px;">
                 </div>
             </div>
 
@@ -64,11 +66,11 @@ $this->title ='🤖GundamCenter';
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6" >
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2>NEWS by Tim Creator</h2>
-                    <p class="lead">Berita ini berisi konten tentang seputar Mobile suit Gundam dan informasi yang ditampilkan disajikan dalam bentuk sebuah artikel atau buletin <a target="_blank" href="index.php?r=news%2Findex">News</a> Tidak ada unsur politik. </p>
+                    <h2>Berita</h2>
+                    <p class="lead">Berita mengenai perkembangan news gundam yang dapat di akses oleh user dengan link  <a target="_blank" href="index.php?r=news%2Findex">ini</a>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                    <img class="img-responsive" src="https://image.freepik.com/free-vector/visual-data-concept-illustration_114360-1713.jpg" alt="" style="width:442px;height:440px;">
+                    <img class="img-responsive" src="https://image.freepik.com/free-vector/newsletter-subscription-modern-pastime-online-news-reading-internet-mail-spam-advertisement-phishing-letter-scam-idea-design-element_335657-3546.jpg" alt="" style="width:442px;height:440px;">
                 </div>
             </div>
 
@@ -87,7 +89,7 @@ $this->title ='🤖GundamCenter';
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2>Tentang GundamCenter™</h2>
+                    <h2>Perihal website</h2>
                     <p class="lead">Untuk mengenal lebih jauh silahkan klik <a target="_blank" href="index.php?r=site%2Fabout">link</a> ini.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
